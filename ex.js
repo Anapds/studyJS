c7785716a9ecda8e102afc165922e1967903184f
Anapds <paulatlgd@gmail.com>
2024-08-12 19:47:04 -0300

    curso js

---
+++ ex.js
@@ -0,0 +1,29 @@
+// escreva uma função que recebe dois numeros e retorne o maior deles 
+
+// const max = (x, y) => x > y ? x : y;
+// console.log(max(10, 5));
+
+// #################################################################
+ 
+
+// jeito que fiz 
+
+// const A = 10;
+// const B = 20;
+// const C = 30;
+
+// const soma = A + B 
+
+// if (soma > C) {
+//     console.log('a constante c é menor')
+// } else {
+//     console.log('a constante c e maior')
+// }
+
+// jeito simplificado 
+
+// const A = 10;
+// const B = 20;
+// const C = 30;
+
+// console.log(soma > C ? 'a constante c é menor' : 'a constante c é maior');
